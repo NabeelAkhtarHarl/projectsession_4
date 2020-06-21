ca125ebcdac37b2f69a544ca883d08460da28fcb
NabeelAkhtarHarl <66417858+NabeelAkhtarHarl@users.noreply.github.com>
2020-06-21 18:22:51 +0500

    type code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+ //import React  from 'react';
+ import React, {useState, Fragment} from 'react';
+ import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ function App (){
+  const [name,setName]=useState("Nabeel");
+ const [count,setCount]=useState(1);
+ const [decrement,setDecrement]=useState(1);
+ const [isMorning,setMorning]=useState(false);
+ const handalclick =()=>{
+   setCount(count+1);
+   setName("Umer");
+   setMorning(true);
+ }
+ const handalclick2 =()=>{
+  setDecrement(decrement-1);
+
+ }
+ return(
+   <div className="App-header"> 
+           <h1>Day Time={isMorning ? 'Morning' : 'Night' }</h1>
+          <h1>Yor Name is{name}</h1>
+          <h1 >Your number is {count}</h1>
+          <h1>your decrement number is{decrement}</h1>
+
+          <button type='button' onClick={handalclick}>Chang Name and number</button>
+          <button type="button" onClick={handalclick2}>decrement number</button>
+   </div>
+ )
+
+ }
 
 export default App;
+ 
